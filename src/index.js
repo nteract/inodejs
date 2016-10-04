c7797a4fa39e10023a456e6c702294a7ad44e862
Kyle Kelley <rgbkrk@gmail.com>
2016-10-04 02:08:43 -0700

    Rely on jp-kernel-prebuilt

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 const path = require('path');
-const Kernel = require('jp-kernel');
+const Kernel = require('jp-kernel-prebuilt');
 
 const connectionFile = process.argv[2];
 
